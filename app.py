0bbf7379cc5583301ea52ecc64907a41a53e4502
volitilov <volitilov@gmail.com>
2017-01-21 23:12:19 +0300

    test url_for(), jinja, render_template()

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
 from flask import Flask
 from flask import render_template
 
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 app = Flask(__name__)
 
+# default ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def intrernal_server_error(e):
+	return render_template('500.html'), 500
+
+
+# experements ::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 @app.route('/')
 @app.route('/<name>')
 def index(name=None):
 	return render_template('index.html', name=name)
 
+
+@app.route('/user/<string:username>')
+def user(username):
+	return 'User: {}'.format(username)
+
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 if __name__ == '__main__':
 	app.run()
