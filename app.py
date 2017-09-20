de2ab0d5fcd75137499de4bf6897edf1afe9a14f
volitilov <volitilov@gmail.com>
2017-09-20 19:11:19 +0300

    start lern Flask

--- app.py
+++ app.py
@@ -1,35 +1,76 @@
-from flask import Flask
-from flask import render_template
+import os
 
-# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# flask
+from flask import (
+	Flask, url_for, render_template, session,
+	request, make_response, redirect, flash
+)
+
+# flask_script
+from flask_script import Manager
+
+# flask_wtf
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField
+from wtforms.validators import DataRequired
+
+# flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+			'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
-# default ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+db = SQLAlchemy(app)
 
+# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+class MyForm(FlaskForm):
+	name = StringField('name', validators=[DataRequired()])
+	password = PasswordField('password', validators=[DataRequired()])
+	text = TextAreaField('text', validators=[DataRequired()])
+
+
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+	name, password, text = None, None, None
+	form = MyForm()
+	if form.validate_on_submit():
+		old_name = session.get('name')
+		if old_name is not None and old_name != form.name.data:
+			flash('This name: {} - new'.format(form.name.data))
+		session['name'] = form.name.data
+		form.name.data = '' 
+		return redirect(url_for('index'))
+
+	return render_template('index.html', 
+						form=form, 
+						name=session.get('name'))
+
+
+
+# errors :::::::::::::::::::::::::::::::::::::::::::::
 @app.errorhandler(404)
-def page_not_found(e):
+def page_not_found(error):
 	return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
-def intrernal_server_error(e):
+def internal_server_error(error):
 	return render_template('500.html'), 500
+# end errors :::::::::::::::::::::::::::::::::::::::::
 
 
-# experements ::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-@app.route('/')
-@app.route('/<name>')
-def index(name=None):
-	return render_template('index.html', name=name)
-
-
-@app.route('/user/<string:username>')
-def user(username):
-	return 'User: {}'.format(username)
-
-# ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+manager = Manager(app)
 
 if __name__ == '__main__':
-	app.run()
+	app.debug = True
+	manager.run()
